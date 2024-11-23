e91c056e07743bded59a295fb95222728897c5c8
Bahruz Balaevi <bbalaevi@credo.ge>
2024-11-24 01:53:47 +0400

    Branch_First_Commit

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,2 +1,18 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FirstClass {
+
+    @Test
+    public void testLogin () throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://test.css.credo.ge/");
+
+        System.out.println("Page title is: " + driver.getTitle());
+        Thread.sleep(3000);
+        driver.close();
+    }
 }
